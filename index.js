55eb98313213175a3000a5e72d3320c3388e90a2
shahid66 <kk.shahid66@gmail.com>
2022-11-19 14:37:23 +0600

    first commit

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
-const express=require('express');
+const express = require("express");
 const app = express();
-const helmet = require('helmet');
+const helmet = require("helmet");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const morgan = require("morgan");
-const cors = require('cors');
-const { readdirSync } = require("fs");
-const router = require('./src/routes/api');
-const path = require('path');
+const cors = require("cors");
 
-
+const router = require("./src/routes/api");
+const path = require("path");
 
 // middlewares
-app.use(helmet())
-app.use(express.static('public'));
-app.use(express.json({limit:"200mb"}));
+app.use(helmet());
+
+app.use(express.json({ limit: "200mb" }));
 
 app.use(express.urlencoded({ limit: "200mb", extended: true }));
-
 
 app.use(morgan("dev"));
 app.use(cors());
 
 // DB Connection
 mongoose
-    .connect(process.env.DATABASE)
-    .then(() => console.log("DB connected successfully"))
-    .catch((err) => console.log("DB Error => ", err));
+  .connect(process.env.DATABASE)
+  .then(() => console.log("DB connected successfully"))
+  .catch((err) => console.log("DB Error => ", err));
 
 // routes middleware
-app.use(router)
-
-app.use(express.static('client/build'))
-app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-})
+app.use(router);
 
 // server
 const port = process.env.PORT || 9000;
 
 app.listen(port, () => {
-    console.log(`App is  running on port ${port}`);
+  console.log(`App is  running on port ${port}`);
 });
--- src/controllers/testController.js
+++
@@ -1,3 +0,0 @@
-exports.getTestData=(req,res)=>{
-    res.status(200).json({status:"success",data:"hello"})
-}
